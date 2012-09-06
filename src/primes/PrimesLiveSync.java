d393d0eec2bdaa64126fcb04e789ddca7a9ad112
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-09-06 16:18:57 -0500

    MP6 - exercise: PrimesLiveSync

---
+++ src/primes/PrimesLiveSync.java
@@ -0,0 +1,35 @@
+package primes;
+
+import util.UnimplementedExercise;
+
+/* 
+ Steps:
+
+ 1. Copy your or the given solution to PrimesLive to the class below. We keep the 
+ 	original so that we can compare results.
+
+ 2. Fix the races using synchronization.
+ */
+
+public class PrimesLiveSync extends PrimesComputation implements
+		LiveResults<Integer[]>, UnimplementedExercise {
+
+	@Override
+	public Boolean[] computePrimes(int upto) {
+		// TODO: implement this method
+		return null;
+	}
+
+	@Override
+	public Integer[] getResults() {
+		// TODO implement this method
+		return null;
+	}
+
+	@Override
+	public int resultsCount() {
+		// TODO implement this method
+		return 0;
+	}
+
+}
