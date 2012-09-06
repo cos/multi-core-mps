3b11ddd3aab1c797eb32ab5b4f3467fec614bb20
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-09-06 17:24:23 -0500

    MP7 - exercise: PrimesLiveAtomic

---
+++ src/primes/PrimesLiveAtomic.java
@@ -0,0 +1,35 @@
+package primes;
+
+import util.UnimplementedExercise;
+
+/* 
+ Steps:
+
+ 1. Copy your or the given solution to PrimesLive to the class below. We keep the 
+ 	original so that we can compare results.
+
+ 2. Fix the races by replacing the array index with an AtomicInteger.
+ */
+
+public class PrimesLiveAtomic extends PrimesComputation implements
+		LiveResults<Integer[]>, UnimplementedExercise {
+
+	@Override
+	public Boolean[] computePrimes(int upto) {
+		// TODO: implement this method
+		return null;
+	}
+
+	@Override
+	public Integer[] getResults() {
+		// TODO implement this method
+		return null;
+	}
+
+	@Override
+	public int resultsCount() {
+		// TODO implement this method
+		return 0;
+	}
+
+}
