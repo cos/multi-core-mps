d21c31353d8a951905630936b531ed15698395ed
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-09-20 21:52:50 -0500

    MP5 - exercise: LiveValue javadoc

--- src/pi/LiveValue.java
+++ src/pi/LiveValue.java
@@ -1,5 +1,9 @@
 package pi;
 
 public interface LiveValue {
+	/**
+	 * Return the most up-to-date approximation. This method can be invoked
+	 * at any time from outside the implementing object.
+	 */
 	public double liveValue();
 }
