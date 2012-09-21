695cb43635969676b72fdb1a93586ae1ff0a1b16
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-09-20 21:53:19 -0500

    MP5 - exercise: updated the instructions, add javadoc

--- src/pi/PiLive.java
+++ src/pi/PiLive.java
@@ -12,30 +12,39 @@ import util.UnimplementedExercise;
  */
 
 /*
-   	The goal of this exercise is to show how easy threads can communicate but
-	also how easy it is to introduce concurrency bugs.
+ * 
+ *  In this exercise, we make the Pi example slightly more complex. 
+ 	Let's say we want a client of our class to be able to observe the most 
+ 	up-to-date approximation of pi as the computation progresses. 
 
 	Steps:
 
- 1. We start from the threads version of this exercise. Copy either your or
-	the given threads solution to the class below. Merge as necessary.
+ 1. We start from the threads version of this exercise. Replace the computePi
+  	below with the computePi and the companion inner class from either 
+  	your or the given threads solution.
 
  2. Gather approximate results from the threads in a shared data structure.
 	Make it possible for other concurrent threads to inspect the most up to date 
 	value by calling the liveValue() method.
-
- 3. Print out the live of Pi 100ms second after starting the threads, and
-	before the join()s. It will be printed three times due to the warm-up.
+	
+ 3. Run PiTest. You might notice that the live approximation is slightly skewed. 
+    This is not only because the computation is not over, but also because of 
+    race conditions. Do not add synchronization here. The next exercise (PiLiveSync) 
+    will ask you to introduce synchronization.
 */
 
 public class PiLive implements PiApproximation, LiveValue, UnimplementedExercise {
 
 	@Override
 	public double computePi(long iterations) {
-		// TODO: implement
+		// TODO: replace with your implementation 
 		return 100.0;
 	}
 
+	/**
+	 * Return the most up-to-date Pi approximation. This method can be invoked at any time
+	 * from outside the PiLive object.
+	 */
 	public double liveValue() {
 		// TODO: implement
 		return 100.0;
