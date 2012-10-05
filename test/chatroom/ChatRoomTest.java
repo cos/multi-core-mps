d1231d96477eb5892a4b2a51e3abaf7c18ef1761
Cosmin Radoi <cosmin.radoi@gmail.com>
2012-10-05 16:16:42 -0500

    ChatRoomTest

---
+++ test/chatroom/ChatRoomTest.java
@@ -0,0 +1,10 @@
+package chatroom;
+
+/*
+ * Write sleep-based tests revealing the atomicity violations in the ChatRoom class. 
+ * Test each method of the class.
+ */
+
+public class ChatRoomTest {
+
+}
